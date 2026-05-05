637669067b3853df7113b2a83d482ec029f843ff
joaoneto9 <joaonetocg1@gmail.com>
2026-05-05 17:09:29 -0300

    fix: remove .env.local file

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from pathlib import Path
 from langchain_ollama import ChatOllama
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 
+MODEL_ID = "gemma4:e4b"
+THINKING = True
+
 llm = ChatOllama(
-    model="gemma4:e4b",
-    reasoning=True, # Obs: se o modelo usa o reasoning por padrao -> None: <think> label aparecera na resposta, True: removera.
+    model=MODEL_ID,
+    reasoning=THINKING, # Obs: se o modelo usa o reasoning por padrao -> None: <think> label aparecera na resposta, True: removera.
     temperature=0.7,
     top_k=35,
     top_p=0.9
 )
 
+def get_model_id():
+    return MODEL_ID
+
 def send_to_gemma4(images_patient) -> dict:
     system_prompt = """
-Você é um médico radiologista sênior. Sua tarefa é realizar uma análise sistemática e rigorosa de radiografias de tórax, garantindo a detecção de alterações vasculares, cardíacas e estruturais (incluindo achados degenerativos e crônicos).
+Você é um médico radiologista sênior. Sua tarefa é realizar uma análise sistemática e rigorosa de radiografias de tórax.
 
 DIRETRIZES DE ANÁLISE (Siga rigorosamente esta ordem):
 
@@ -22,25 +28,24 @@ DIRETRIZES DE ANÁLISE (Siga rigorosamente esta ordem):
 1. Ossos e Partes Moles: Checar fraturas em costelas, clavículas e coluna; avaliar cúpulas diafragmáticas e buscar enfisema subcutâneo ou sombras mamárias.
 2. Vias Aéreas: Avaliar se a traqueia está centralizada ou desviada e a perviedade dos brônquios principais.
 3. Pulmões e Pleura: Buscar opacidades (nódulos, massas, consolidações) ou pneumotórax; checar se os seios costofrênicos estão livres ou velados (derrame).
-4. Coração e Mediastino: Avaliar se há cardiomegalia (ICT > 50%) e checar a anatomia do mediastino, botão aórtico e hilos.
+4. Coração e Mediastino: Meça explicitamente o ICT. Avaliar se há cardiomegalia (ICT > 50%) e checar a anatomia do mediastino, botão aórtico e hilos.
 5. Dispositivos (Se houver): Descrever presença e posicionamento de acessos, cateteres, tubos ou marca-passos.
 6. Descrição das Alterações: Se houver achados anormais, descrever o tipo de lesão e a localização exata (ex: "opacidade em terço inferior do pulmão direito").
 
-REGRAS DE FORMATO DA RESPOSTA:
-- Use exatamente: '- N. Título da Categoria: Descrição'.
+REGRAS DE FORMATO DAS ANÁLISES (OBRIGATÓRIO):
+- Use exatamente: '- Título da Categoria: Descrição'.
 - Responda APENAS com os bullet points.
 - Idioma: Português do Brasil.
 
 RESUMO GERAL (OBRIGATÓRIO):
 - No fim da análize de todos os pontos (0 a 6) realize um resumo geral da análise no seguinte formato.
-- O início dessa etapa deve ser acompanhada do 'título': RADIOGRAFIA DO TÓRAX – PA E PERFIL.
-- Em seguida informe esses aspectos em bullet points ('-') de forma objetiva (OBRIGATÓRIA):
+- Em seguida informe esses aspectos em bullet points ('-') de forma objetiva:
     1. Indique as anomalias nos Ossos e nas Partes Moles, caso não apresente indique que a estruturas ósseas não apresenta alteração. (análise 1)
     2. Indique a situação da traqueia. (análise 2)
     3. Indique a perviedade dos brônquios principais. (análise 2)
     4. Indique como está a transparência dos pulmões. (análise 3)
     5. Indique se os seios costofrênicos estão livres ou velados. (análise 3)
-    6. Indique um valor estimado do ICT e se ele está aumentado. Caso não esteja, sinalizar a normalidade. (análise 4)
+    6. Indique o valor do ICT e se ele está aumentado. Caso não esteja, sinalizar a normalidade. (análise 4)
     7. Indique como está a anatomia do mediastino, botão aórtico e hilos. (análise 4)
     8. Indique (se houver) a presença de dispositivos externos. Caso não apresente, não cite nada. (análise 5)
     9. Por fim, mencione os achados anormais com base em sua análise. (análise 6)
@@ -57,15 +62,24 @@ RESUMO GERAL (OBRIGATÓRIO):
         HumanMessage(user_prompt)
     ] 
 
-    response = llm.invoke(messages, reasoning=True)
+    response = llm.invoke(messages, reasoning=THINKING)
     
     response_content = response.content
-    reasoning = response.additional_kwargs.get("reasoning_content")
 
+    print(response_content)
+    print("\n\n============================\n\n")
+
+    if THINKING:
+        reasoning = response.additional_kwargs.get("reasoning_content")
+
+        return {
+            "reasoning": reasoning,
+            "response": response_content
+        }
+    
     return {
-        "reasoning": reasoning,
         "response": response_content
-    }
+        }
 
 def get_images_jpg() -> dict:
     path = Path.home() / "Downloads" / "HUAC-DICOM-TORAX-DCM" / "jpg-images" # obs: para usar path.home() tem que usar / para separa os diretorios
@@ -105,5 +119,5 @@ if __name__ == "__main__":
     dict_responses = images_patient_responses()
     df = pd.Series(dict_responses).to_frame(name="Gemma4-x-ray-findings")
 
-    df.to_json('gemma4_x_ray_findings.json', orient='index', indent=4, force_ascii=False)
+    df.to_json(f'{MODEL_ID}_x_ray_findings.json', orient='index', indent=4, force_ascii=False)
     
--- scripts/script_join_json.py
+++ scripts/script_join_json.py
@@ -2,15 +2,17 @@ import json
 import pandas as pd
 from pathlib import Path
 
+from main import get_model_id
+
 def read_file(path): 
     with open(path, 'r') as file:
         archive = json.load(file)
         return archive 
 
 if __name__ == "__main__":
-    path_to_save =  Path.home() / "Downloads" / "HUAC-DICOM-TORAX-DCM" / "model_x_reports.csv"
+    path_to_save =  Path.home() / "Downloads" / "HUAC-DICOM-TORAX-DCM" / f"model_{get_model_id()}_x_reports.csv"
     reports_json_path = Path.home() / "Downloads" / "HUAC-DICOM-TORAX-DCM" / "patients_reports.json"
-    gemma4_findings_json_path = "../gemma4_x_ray_findings.json"
+    gemma4_findings_json_path = f"../{get_model_id()}_x_ray_findings.json"
 
     reports_json = read_file(reports_json_path)
     gemma4_findings_json = read_file(gemma4_findings_json_path)
